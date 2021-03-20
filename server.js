45a30ccccd49e96f1e9859fb65b9c587aa7f295b
Matt Gross <mbgross111@gmail.com>
2021-03-20 12:42:41 -0700

    change sequalize.sync({ force: true })
    :q

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.set('view engine', 'handlebars');
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`application live at http://localhost:${PORT}/`));
 });
