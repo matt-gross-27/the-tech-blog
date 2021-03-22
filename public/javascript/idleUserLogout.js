9ddf95267a3181e2f686bcc7c2d0a75687dad800
Matt Gross <mbgross111@gmail.com>
2021-03-22 00:27:52 -0700

    logout idle users after 30 minutes

---
+++ public/javascript/idleUserLogout.js
@@ -0,0 +1,32 @@
+let timer;
+
+async function logout() {
+  const response = await fetch(`${window.location.origin}/api/users/logout`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+function resetTimer() {
+  console.log(`moving`)
+  clearTimeout(timer);
+  timer = setTimeout(logout, 1800000);
+}
+
+
+function startTimer() {
+  timer = setTimeout(logout, 1800000);
+}
+
+startTimer();
+
+document.addEventListener('click', resetTimer);
+document.addEventListener('scroll', resetTimer);
+
+// log out after 30 minutes of not clicking or scrolling
