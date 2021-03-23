ba99b1dc280923bde043189b75e42f09fb474c0a
Matt Gross <mbgross111@gmail.com>
2021-03-22 18:34:34 -0700

    Handle errors in search user form

--- public/javascript/searchUsers.js
+++ public/javascript/searchUsers.js
@@ -1,12 +1,32 @@
 const $searchForm = document.querySelector("#search-form");
+const $usersDatalist = document.querySelector("#users");
+let userData;
+
+async function getUsers() {
+  const response = await fetch(`${window.location.origin}/api/users`, {
+    method: 'GET',
+  });
+
+  userData = await response.json();
+
+  userData.forEach(user => {
+    const optionEl = document.createElement('option');
+    optionEl.value = user.username;
+    $usersDatalist.appendChild(optionEl)
+  });
+}
 
 searchFormHandler = (event) => {
   event.preventDefault();
 
   const username = document.querySelector("#search-username").value;
-  if (username) {
+  if (userData.includes(username)) {
     document.location.replace(`/${username}`);
+  } else {
+    document.querySelector("#search-username").value = null
+    document.querySelector("#search-username").setAttribute("placeholder", "User not found");
   }
 }
 
+getUsers();
 $searchForm.addEventListener('submit', searchFormHandler)
