fef70f8a907c025dc84734fd066c5e8e361984aa
Matt Gross <mbgross111@gmail.com>
2021-03-22 19:01:18 -0700

    remove shareButtons if not compatible with browser

--- public/javascript/share.js
+++ public/javascript/share.js
@@ -3,29 +3,33 @@ const $shareButtons = document.querySelectorAll('.oi-shareDiv');
 shareButtonHandler = (event) => {
   event.preventDefault();
 
-  let id;
+  let eTarget;
 
   if (event.target.localName === "span") {
-    id = event.target.parentElement.getAttribute("data-post_id");
+    eTarget = event.target.parentElement;
   } else {
-    id = event.target.getAttribute("data-post_id");
+    eTarget = event.target;
   }
+
+  const id = eTarget.getAttribute("data-post_id")
 
   let shareText = document.querySelector(`#blog-post-${id}`).textContent
 
-  if(navigator.share !== undefined) {
-    navigator.share({
-        title: `The Tech Blog`, 
-        text: `The Tech Blog: ${shareText.trim()}
-        `, 
-        url: `${window.location.origin}/posts/${id}`
-    })
-      .then(() => console.log('Successful share'))
-      .catch(err => console.log('Error sharing:', err));
+  navigator.share({
+      title: `The Tech Blog`, 
+      text: `The Tech Blog: ${shareText.trim()}`, 
+      url: `${window.location.origin}/posts/${id}`
+  })
+    .then(() => console.log('Successful share'))
+    .catch(err => console.log('Error sharing:', err));
+}
+
+function showHideShareButtons () {
+  if (navigator.share !== undefined) {
+    $shareButtons.forEach(shareButton => shareButton.addEventListener('click', shareButtonHandler));
   } else {
-    alert(`Sorry. This feature is not compatible with your browser :(`)
+    $shareButtons.forEach(shareButton => shareButton.remove());
   }
 }
 
-
-$shareButtons.forEach(shareButton => shareButton.addEventListener('click', shareButtonHandler));
+showHideShareButtons();
